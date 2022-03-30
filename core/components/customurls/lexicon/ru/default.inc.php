d7557957c0b22197eab60eb2337db8393cfff754
dramspro <dmitry@mspektr.ru>
2022-03-30 23:53:18 +0300

    Add RU translation
    Original: https://forums.modx.com/thread/78843/support-comments-for-customurls?page=2#dis-post-556792

---
+++ core/components/customurls/lexicon/ru/default.inc.php
@@ -0,0 +1,23 @@
+<?php
+$_lang['customurls'] = 'Custom URLs';
+$_lang['customurls.menu_desc'] = 'Определите свой собственный псевдоним и шаблоны URI';
+$_lang['customurls.customurls_err_nf'] = 'Правило не найдено';
+$_lang['customurls.customurls_err_ns'] = 'Правило не указано.';
+$_lang['customurls.customurls_err_nfs'] = 'Правило не найдено и не указано.';
+$_lang['customurls.customurls_err_generate'] = 'Произошла ошибка при попытке повторно создать пользовательские URL-адреса.';
+$_lang['customurls.customurls_err_remove'] = 'Произошла ошибка при попытке удалить правило.';
+$_lang['customurls.customurl_create'] = 'Добавить правило';
+$_lang['customurls.customurl_generate'] = 'Обновить пользовательские URL-адреса';
+$_lang['customurls.customurl_generate_confirm'] = 'Вы уверены, что хотите повторно создать пользовательские URL-адреса? Это переопределит существующие пользовательские правила.';
+$_lang['customurls.customurl_update'] = 'Обновить';
+$_lang['customurls.customurl_remove'] = 'Удалить';
+$_lang['customurls.customurl_remove_confirm'] = 'Вы уверены, что хотите удалить это правило?';
+$_lang['customurls.management'] = 'Пользовательские URL-Адреса';
+$_lang['customurls.management_desc'] = 'Это позволяет определить пользовательские шаблоны псевдонимов или URI для ресурсов. Он поддерживает пакеты Translit и Redirector. Вы можете построить свои шаблоны из полей ресурсов, TV полей, фрагментов и выходных фильтров и установить некоторые ограничения, как вы сделали бы с пользовательскими формами. Например, с помощью CustomURLs можно добавить идентификатор ресурса или месяц публикации в псевдонимах всех ресурсов или только для тех, чей родитель = 1 или шаблон = 1. Документация: http://rtfm.modx.com/display/ADDON/CustomUrls';
+$_lang['customurls.pattern'] = 'Паттерн (шаблон URL-адреса) Пример: [[+id]]-[[+alias]]';
+$_lang['customurls.criteria_key'] = 'Поле ограничения (Может быть полем ресурса: ИД (id), родитель (parent), шаблон (template)...)';
+$_lang['customurls.criteria_value'] = 'Ограничение';
+$_lang['customurls.usergroup'] = 'Группа пользователей (Для которой действует правило)';
+$_lang['customurls.uri'] = 'URI (По-умолчанию, создаются псевдонимы, но вы можете вместо этого выбрать создание URI)';
+$_lang['customurls.override'] = 'Перезапись (Перезапись псевдонима или URI при создании ресурса.)';
+$_lang['customurls.active'] = 'Активный';
